6b8152e9847c99da18dcd774cb9d6abf11ccdc1c
Bikash Nath <splittergame91@gmail.com>
2022-10-01 19:08:02 +0530

    Update

--- 1 Recursion -1/first_index_num.py
+++ 1 Recursion -1/first_index_num.py
@@ -4,6 +4,15 @@ def firstIndex(a,n,i):
     return firstIndex(a,n,i+1)
 
     
+def first_index(a,n):
+    if len(a)==0:return -1
+    if a[0]==n:return 0
+    res = first_index(a[1:],n)
+    if res==-1:return -1
+    else:return res+1
+
+
 a= [3,4,5,3,5,7,4,8,9,9,3,7]
 n=int(input('Enter num to check its index: '))
 firstIndex(a,n,0)
+print(first_index(a,n))
