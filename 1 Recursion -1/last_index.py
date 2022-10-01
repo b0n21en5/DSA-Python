16f1bc83e6ddb8f20111c51f27c3d673147e1839
Bikash Nath <splittergame91@gmail.com>
2022-10-01 21:04:11 +0530

    Update

--- 1 Recursion -1/last_index.py
+++ 1 Recursion -1/last_index.py
@@ -13,7 +13,27 @@ def last_index(a, n):
     else:return res
 
 
+def lastIndexB(a,n,i):
+    if i==len(a):return -1
+    res = lastIndexB(a,n,i+1)
+    if res!=-1:return res
+    else:
+        if a[i]==n:return i
+        else:return -1
+
+
+def last_indexB(a,n):
+    if len(a)==0:return -1
+    res = last_indexB(a[1:],n)
+    if res !=-1:return res+1
+    else:
+        if a[0]==n:return 0
+        else:return -1
+
+
 a = [3, 4, 1, 5, 3, 5, 7, 15, 4, 8, 9, 9, 3, 7]
 n = int(input('Enter num to check its index: '))
 lastIndex(a, n, len(a) - 1)
 print(last_index(a, n))
+print(lastIndexB(a,n,0))
+print(last_indexB(a,n))
