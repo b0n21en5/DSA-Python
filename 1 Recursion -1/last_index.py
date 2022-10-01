647ec8d13b8dff747770052ff39bc8d63d1c1ae1
Bikash Nath <splittergame91@gmail.com>
2022-10-01 18:55:34 +0530

    Update

--- 1 Recursion -1/last_index.py
+++ 1 Recursion -1/last_index.py
@@ -1,9 +1,9 @@
-def firstIndex(a,n,i):
+def lastIndex(a,n,i):
     if i<0:return print('num is not the in list')
     if a[i]==n:return print(i)
-    return firstIndex(a,n,i-1)
+    return lastIndex(a,n,i-1)
 
     
 a= [3,4,1,5,3,5,7,15,4,8,9,9,3,7]
 n=int(input('Enter num to check its index: '))
-firstIndex(a,n,len(a)-1)
+lastIndex(a,n,len(a)-1)
