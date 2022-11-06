534c036543c4ea0ae9c933fed345d21dfacb4020
Bikash Nath <splittergame91@gmail.com>
2022-11-06 11:18:28 +0530

    update

--- 1 Recursion -1/sum_array.py
+++ 1 Recursion -1/sum_array.py
@@ -1,10 +1,10 @@
-s=0
+
 def sum_arr(a):
-    global s
-    if len(a)==0:return print(s)
-    s+=a[0]
-    sum_arr(a[1:])
+
+    if len(a)==0:return 0
+
+    return a[0] + sum_arr(a[1:])
     
 n=int(input())
 a=[int(n) for n in input().split(' ',n-1)]
-sum_arr(a)
+print(sum_arr(a))
